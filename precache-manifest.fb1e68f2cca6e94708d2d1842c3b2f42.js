227afe599863f88fdfeff30b922d485cdd3b6a17
Ravi Chalmela <jcravi@gmail.com>
2020-11-22 20:40:48 -0800

    Updates

---
+++ precache-manifest.fb1e68f2cca6e94708d2d1842c3b2f42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e33d70a6dda3de055f2f54652e50bdc",
+    "url": "/clinic/index.html"
+  },
+  {
+    "revision": "c0dab5b90a1cfbe72850",
+    "url": "/clinic/static/css/main.d0240a54.chunk.css"
+  },
+  {
+    "revision": "11f1d0dabd4f866d8968",
+    "url": "/clinic/static/js/2.6022a2ab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/clinic/static/js/2.6022a2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0dab5b90a1cfbe72850",
+    "url": "/clinic/static/js/main.23cba0be.chunk.js"
+  },
+  {
+    "revision": "988d3a03875620f8be6c",
+    "url": "/clinic/static/js/runtime-main.cc94648b.js"
+  }
+]);
